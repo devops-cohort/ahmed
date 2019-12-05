0874194da6ca6437348ec5eaaeb0a9aa90bcb159
ahmed Ibrahim <ahmed.ibrahim@academytrainee.com>
2019-12-05 23:35:04 +0000

    upload from previous repo

---
+++ application/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+from flask_bcrypt import Bcrypt 
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']=('mysql+pymysql://' + getenv('MYSQL_USER') + ':' + getenv('MYSQL_PASSWORD') + '@' + getenv('MYSQL_HOST') + '/' + getenv('MYSQL_DB'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'Secret'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+
+
+
+from application import routes
